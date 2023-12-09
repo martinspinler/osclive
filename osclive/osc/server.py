c9b8ca1f15d9f4b383e77c817184fd82a99aca4f
Martin Spinler <spinler@cesnet.cz>
2023-12-09 10:33:55 +0100

    osc server: add bundle() context manager to RequestHandler

--- osclive/osc/server.py
+++ osclive/osc/server.py
@@ -8,14 +8,37 @@ from typing import List, Tuple, Union, Any, Iterable
 
 from pythonosc import osc_packet
 from pythonosc.osc_message_builder import OscMessageBuilder
+from pythonosc.osc_bundle_builder import OscBundleBuilder, IMMEDIATELY
 
 from zeroconf import ServiceBrowser, ServiceInfo, Zeroconf
 
 
 class ThreadedTCPOSCRequestHandler(socketserver.BaseRequestHandler):
+    class BundleManager:
+        def __init__(self, request_handler):
+            self._rh = request_handler
+
+        def __enter__(self):
+            if self._rh._bundle_inner == 0:
+                self._rh._bundle = None
+                self._rh._bundle = []
+            self._rh._bundle_inner += 1
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self._rh._bundle_inner -= 1
+            if self._rh._bundle_inner == 0:
+                if self._rh._bundle:
+                    self._rh.send_bundle(self._rh._bundle)
+
+                self._rh._bundle = None
+
     def setup(self):
         super().setup()
         print("OSC TCP client connected", self.client_address)
+
+        self._bundle = None
+        self._bundle_inner = 0
+
         self.server.clients.append(self)
 
     def _recv(self, size):
@@ -48,6 +71,16 @@ class ThreadedTCPOSCRequestHandler(socketserver.BaseRequestHandler):
     def handle_message(self, address, params):
         pass
 
+    def bundle(self):
+        return self.BundleManager(self)
+
+    def send_bundle(self, msg_list):
+        bb = OscBundleBuilder(IMMEDIATELY)
+        for msg in msg_list:
+            bb.add_content(msg)
+        bundle = bb.build()
+        self.request.sendall(bundle.size.to_bytes(length=4, byteorder='little') + bundle.dgram)
+
     def send_message(self, address: str, value: Union[int, float, bytes, str, bool, tuple, list]) -> None:
         builder = OscMessageBuilder(address=address)
         if value is None:
@@ -59,10 +92,14 @@ class ThreadedTCPOSCRequestHandler(socketserver.BaseRequestHandler):
         for val in values:
             builder.add_arg(val)
         msg = builder.build()
-        try:
-            self.request.sendall(msg.size.to_bytes(length=4, byteorder='little') + msg._dgram)
-        except:
-            pass
+
+        if self._bundle == None:
+            try:
+                self.request.sendall(msg.size.to_bytes(length=4, byteorder='little') + msg._dgram)
+            except:
+                pass
+        else:
+            self._bundle.append(msg)
 
 
 class DispatchedOSCRequestHandler(ThreadedTCPOSCRequestHandler):
